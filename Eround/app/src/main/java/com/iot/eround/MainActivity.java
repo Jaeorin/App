d69e8856f0ea2b746693d4d66703b9d682fb4779
jaeorin <jaeorin@gmail.com>
2019-05-21 15:59:37 +0900

    상태바 투명 해결

--- Eround/app/src/main/java/com/iot/eround/MainActivity.java
+++ Eround/app/src/main/java/com/iot/eround/MainActivity.java
@@ -1,9 +1,13 @@
 package com.iot.eround;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -29,8 +33,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
 
         Log.d(TAG, "onCreate()");
 
